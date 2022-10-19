afc6fd25490cf69683f2b1603e9676f8167d3223
Tyler Mayfield <tyler.mayfield23@gmail.com>
2022-10-19 12:06:15 -0700

    basic components set up

---
+++ feedback-app/src/components/FeedbackItem.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Card from './shared/Card';
+
+function FeedbackItem({item}) {
+    
+    return (
+        <div className="card">
+            <div className="num-display">{item.rating}</div>
+            <div className="text-display">
+                {item.text}
+            </div>
+        </div>
+  )
+}
+
+export default FeedbackItem
